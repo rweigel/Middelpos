9678d62cb939383dec25ee7f32d300c9db679e9d
Robert Weigel <rweigel@gmu.edu>
2020-06-22 11:41:57 -0400

    figures

---
+++ cpfigs.sh
@@ -0,0 +1,8 @@
+D=../../figures/KAP103_Middelpos
+
+cp -v ${D}/SN-E_x.pdf figures/
+cp -v ${D}/SN-E_y.pdf figures/
+cp -v ${D}/Z_xy_Magnitude_Phase.pdf figures/
+cp -v ${D}/Z_yx_Magnitude_Phase.pdf figures/
+cp -v ${D}/Z_xx_Magnitude_Phase.pdf figures/
+cp -v ${D}/Z_yy_Magnitude_Phase.pdf figures/
