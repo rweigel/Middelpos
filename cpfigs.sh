ed443d83a2ec239f9683c420373e4409bfe94619
Bob Weigel <rweigel@gmu.edu>
2020-07-10 10:14:04 -0400

    Adding figures

--- cpfigs.sh
+++ cpfigs.sh
@@ -1,8 +1,13 @@
-D=../../figures/KAP103_Middelpos
-
-cp -v ${D}/SN-E_x.pdf figures/
-cp -v ${D}/SN-E_y.pdf figures/
-cp -v ${D}/Z_xy_Magnitude_Phase.pdf figures/
-cp -v ${D}/Z_yx_Magnitude_Phase.pdf figures/
-cp -v ${D}/Z_xx_Magnitude_Phase.pdf figures/
-cp -v ${D}/Z_yy_Magnitude_Phase.pdf figures/
+cp ../map/map.pdf figures/
+cp ../figures/KAP103/SN_compare-E_x.pdf figures/
+cp ../figures/KAP103/SN_compare-E_y.pdf figures/
+cp ../figures/KAP103/transferfnZ_compare-Z_xx_Magnitude_Phase.pdf figures/
+cp ../figures/KAP103/transferfnZ_compare-Z_xy_Magnitude_Phase.pdf figures/
+cp ../figures/KAP103/transferfnZ_compare-Z_yx_Magnitude_Phase.pdf figures/
+cp ../figures/KAP103/transferfnZ_compare-Z_yy_Magnitude_Phase.pdf figures/
+cp ../figures/KAP103_Middelpos/SN_compare-E_x.pdf figures/
+cp ../figures/KAP103_Middelpos/SN_compare-E_y.pdf figures/
+cp ../figures/KAP103_Middelpos/transferfnZ_compare-Z_xx_Magnitude_Phase.pdf figures/
+cp ../figures/KAP103_Middelpos/transferfnZ_compare-Z_xy_Magnitude_Phase.pdf figures/
+cp ../figures/KAP103_Middelpos/transferfnZ_compare-Z_yx_Magnitude_Phase.pdf figures/
+cp ../figures/KAP103_Middelpos/transferfnZ_compare-Z_yy_Magnitude_Phase.pdf figures/
